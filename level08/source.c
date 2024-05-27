1c516bcaf556250f2fd9e36c4ea930d2c7fa8788
Romain Kowalski <romain.kowalski@proton.me>
2024-05-27 10:42:58 +0200

    level08 wip

---
+++ level08/source.c
@@ -0,0 +1,187 @@
+ghidra:
+void log_wrapper(FILE *param_1,char *param_2,char *param_3)
+{
+  char cVar1;
+  size_t sVar2;
+  ulong uVar3;
+  ulong uVar4;
+  char *pcVar5;
+  long in_FS_OFFSET;
+  byte bVar6;
+  undefined8 local_120;
+  char local_118 [264];
+  long local_10;
+  
+  bVar6 = 0;
+  local_10 = *(long *)(in_FS_OFFSET + 0x28);
+  local_120 = param_1;
+  strcpy(local_118,param_2);
+  uVar3 = 0xffffffffffffffff;
+  pcVar5 = local_118;
+  do {
+    if (uVar3 == 0) break;
+    uVar3 = uVar3 - 1;
+    cVar1 = *pcVar5;
+    pcVar5 = pcVar5 + (ulong)bVar6 * -2 + 1;
+  } while (cVar1 != '\0');
+  uVar4 = 0xffffffffffffffff;
+  pcVar5 = local_118;
+  do {
+    if (uVar4 == 0) break;
+    uVar4 = uVar4 - 1;
+    cVar1 = *pcVar5;
+    pcVar5 = pcVar5 + (ulong)bVar6 * -2 + 1;
+  } while (cVar1 != '\0');
+  snprintf(local_118 + (~uVar4 - 1),0xfe - (~uVar3 - 1),param_3);
+  sVar2 = strcspn(local_118,"\n");
+  local_118[sVar2] = '\0';
+  fprintf(local_120,"LOG: %s\n",local_118);
+  if (local_10 != *(long *)(in_FS_OFFSET + 0x28)) {
+                    // WARNING: Subroutine does not return
+    __stack_chk_fail();
+  }
+  return;
+}
+
+
+
+undefined8 main(int param_1,undefined8 *param_2)
+{
+  char cVar1;
+  int __fd;
+  int iVar2;
+  FILE *pFVar3;
+  FILE *__stream;
+  ulong uVar4;
+  undefined8 *puVar5;
+  long in_FS_OFFSET;
+  byte bVar6;
+  char local_79;
+  undefined8 local_78;
+  undefined2 local_70;
+  char local_6e;
+  long local_10;
+  
+  bVar6 = 0;
+  local_10 = *(long *)(in_FS_OFFSET + 0x28);
+  local_79 = -1;
+  if (param_1 != 2) {
+    printf("Usage: %s filename\n",*param_2);
+  }
+  pFVar3 = fopen("./backups/.log","w");
+  if (pFVar3 == (FILE *)0x0) {
+    printf("ERROR: Failed to open %s\n","./backups/.log");
+                    // WARNING: Subroutine does not return
+    exit(1);
+  }
+  log_wrapper(pFVar3,"Starting back up: ",param_2[1]);
+  __stream = fopen((char *)param_2[1],"r");
+  if (__stream == (FILE *)0x0) {
+    printf("ERROR: Failed to open %s\n",param_2[1]);
+                    // WARNING: Subroutine does not return
+    exit(1);
+  }
+  local_78._0_1_ = '.';
+  local_78._1_1_ = '/';
+  local_78._2_1_ = 'b';
+  local_78._3_1_ = 'a';
+  local_78._4_1_ = 'c';
+  local_78._5_1_ = 'k';
+  local_78._6_1_ = 'u';
+  local_78._7_1_ = 'p';
+  local_70._0_1_ = 's';
+  local_70._1_1_ = '/';
+  local_6e = '\0';
+  uVar4 = 0xffffffffffffffff;
+  puVar5 = &local_78;
+  do {
+    if (uVar4 == 0) break;
+    uVar4 = uVar4 - 1;
+    cVar1 = *(char *)puVar5;
+    puVar5 = (undefined8 *)((long)puVar5 + (ulong)bVar6 * -2 + 1);
+  } while (cVar1 != '\0');
+  strncat((char *)&local_78,(char *)param_2[1],99 - (~uVar4 - 1));
+  __fd = open((char *)&local_78,0xc1,0x1b0);
+  if (__fd < 0) {
+    printf("ERROR: Failed to open %s%s\n","./backups/",param_2[1]);
+                    // WARNING: Subroutine does not return
+    exit(1);
+  }
+  while( true ) {
+    iVar2 = fgetc(__stream);
+    local_79 = (char)iVar2;
+    if (local_79 == -1) break;
+    write(__fd,&local_79,1);
+  }
+  log_wrapper(pFVar3,"Finished back up ",param_2[1]);
+  fclose(__stream);
+  close(__fd);
+  if (local_10 != *(long *)(in_FS_OFFSET + 0x28)) {
+                    // WARNING: Subroutine does not return
+    __stack_chk_fail();
+  }
+  return 0;
+}
+
+hex-rays:
+unsigned __int64 __fastcall log_wrapper(FILE *a1, const char *a2, const char *a3)
+{
+  char dest[264]; // [rsp+20h] [rbp-110h] BYREF
+  unsigned __int64 v6; // [rsp+128h] [rbp-8h]
+
+  v6 = __readfsqword(0x28u);
+  strcpy(dest, a2);
+  snprintf(&dest[strlen(dest)], 254 - strlen(dest), a3);
+  dest[strcspn(dest, "\n")] = 0;
+  fprintf(a1, "LOG: %s\n", dest);
+  return __readfsqword(0x28u) ^ v6;
+}
+// 4008C4: using guessed type char dest[264];
+
+//----- (00000000004009F0) ----------------------------------------------------
+int __cdecl main(int argc, const char **argv, const char **envp)
+{
+  FILE *v4; // [rsp+28h] [rbp-88h]
+  FILE *stream; // [rsp+30h] [rbp-80h]
+  int fd; // [rsp+38h] [rbp-78h]
+  char buf; // [rsp+3Fh] [rbp-71h] BYREF
+  char dest[104]; // [rsp+40h] [rbp-70h] BYREF
+  unsigned __int64 v9; // [rsp+A8h] [rbp-8h]
+
+  v9 = __readfsqword(0x28u);
+  buf = -1;
+  if ( argc != 2 )
+    printf("Usage: %s filename\n", *argv);
+  v4 = fopen("./backups/.log", "w");
+  if ( !v4 )
+  {
+    printf("ERROR: Failed to open %s\n", "./backups/.log");
+    exit(1);
+  }
+  log_wrapper(v4, "Starting back up: ", argv[1]);
+  stream = fopen(argv[1], "r");
+  if ( !stream )
+  {
+    printf("ERROR: Failed to open %s\n", argv[1]);
+    exit(1);
+  }
+  strcpy(dest, "./backups/");
+  strncat(dest, argv[1], 99 - strlen(dest));
+  fd = open(dest, 193, 432LL);
+  if ( fd < 0 )
+  {
+    printf("ERROR: Failed to open %s%s\n", "./backups/", argv[1]);
+    exit(1);
+  }
+  while ( 1 )
+  {
+    buf = fgetc(stream);
+    if ( buf == -1 )
+      break;
+    write(fd, &buf, 1uLL);
+  }
+  log_wrapper(v4, "Finished back up ", argv[1]);
+  fclose(stream);
+  close(fd);
+  return 0;
+}
